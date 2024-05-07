340795fa9f1f8eed81e438700cc657c5cc0318a0
Daniel Bahrami <daniel@bahrami.dk>
2024-05-07 09:50:06 +0200

    Add dynamic configuration

--- sl/build.rs
+++ sl/build.rs
@@ -1,3 +1,19 @@
+use embuild::cargo::set_rustc_env;
+use embuild::kconfig::{try_from_config_file, Value};
+
 fn main() {
     embuild::espidf::sysenv::output();
+
+    match try_from_config_file("kconfig.projbuild") {
+        Ok(configurations) => {
+            for (key, value) in configurations {
+                if let Value::String(string) = value {
+                    set_rustc_env(&key, &string);
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Failed to load configurations: {:?}", err);
+        }
+    }
 }
